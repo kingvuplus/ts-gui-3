09e4b4322e369fadd99bd99579d9fc8695490cd3
kingvuplus <sabahomri1@outlook.fr>
2016-10-19 17:31:11 +0100

    Update InfoBarGenerics.py

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -124,12 +124,14 @@ class InfoBarDish():
 
     def __init__(self):
         self.dishDialog = self.session.instantiateDialog(Dish)
+	self.dishDialog.setSubScreen()
 
 
 class InfoBarUnhandledKey():
 
     def __init__(self):
         self.unhandledKeyDialog = self.session.instantiateDialog(UnhandledKey)
+	self.unhandledKeyDialog.setSubScreen()
         self.hideUnhandledKeySymbolTimer = eTimer()
         self.hideUnhandledKeySymbolTimer.callback.append(self.unhandledKeyDialog.hide)
         self.checkUnusedTimer = eTimer()
@@ -1185,6 +1187,7 @@ class InfoBarRdsDecoder():
 
     def __init__(self):
         self.rds_display = self.session.instantiateDialog(RdsInfoDisplay)
+	self.rds_display.setSubScreen()
         self.session.instantiateSummaryDialog(self.rds_display)
         self.rass_interactive = None
         self.__event_tracker = ServiceEventTracker(screen=self, eventmap={iPlayableService.evEnd: self.__serviceStopped,
@@ -1612,6 +1615,7 @@ class InfoBarPVRState():
     def __init__(self, screen = PVRState, force_show = False):
         self.onPlayStateChanged.append(self.__playStateChanged)
         self.pvrStateDialog = self.session.instantiateDialog(screen)
+	self.pvrStateDialog.setSubScreen()
         self.onShow.append(self._mayShow)
         self.onHide.append(self.pvrStateDialog.hide)
         self.force_show = force_show
@@ -2129,6 +2133,7 @@ class InfoBarPiP():
                 self.ScreenSaverTimerStart()
         else:
             self.session.pip = self.session.instantiateDialog(PictureInPicture)
+	    self.session.pip.setSubScreen() 
             self.session.pip.show()
             newservice = self.lastPiPService or self.session.nav.getCurrentlyPlayingServiceOrGroup() or slist and slist.servicelist.getCurrent()
             if self.session.pip.playService(newservice):
@@ -3049,6 +3054,7 @@ class InfoBarSubtitleSupport(object):
         self.selected_subtitle = None
         if isStandardInfoBar(self):
             self.subtitle_window = self.session.instantiateDialog(SubtitleDisplay)
+	    self.subtitle_window.setSubScreen()
         else:
             from Screens.InfoBar import InfoBar
             self.subtitle_window = InfoBar.instance.subtitle_window
